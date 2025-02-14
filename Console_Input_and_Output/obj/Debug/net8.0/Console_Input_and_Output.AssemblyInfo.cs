3775532fb2a63ab9bd9c9463d6c8bff0058772ee
levyzaraysky <levyz@piter.org>
2025-02-14 15:03:48 -0800

    startingda

--- Console_Input_and_Output/obj/Debug/net8.0/Console_Input_and_Output.AssemblyInfo.cs
+++ Console_Input_and_Output/obj/Debug/net8.0/Console_Input_and_Output.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console_Input_and_Output")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93c58938eeb72ee7d98378a4a54de241d34d332")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console_Input_and_Output")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console_Input_and_Output")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
